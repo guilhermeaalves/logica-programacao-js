525260ed28aaa19bb8478ea81dcb43ff02c2f813
Guilherme Alves <contatoguilhermealv@gmail.com>
2024-03-16 19:34:25 +0000

    ajustes

--- desafio2.js
+++ desafio2.js
@@ -5,24 +5,14 @@ const imc= (peso / altura **2);
 
 if(imc <= 18.5){
     window.alert(`Abaixo do peso`);
-}
-
-    else if(imc <= 24.9){
+    } else if(imc <= 24.9){
         window.alert(`Peso normal`);
-    }
-
-    else if(imc <= 29.9){
+    } else if(imc <= 29.9){
         window.alert(`Sobrepeso`);
-    }
-
-    else if(imc <= 34.9){
+    } else if(imc <= 34.9){
         window.alert(`Obesidade grau I`);
-    }
-
-    else if (imc <= 39.9){
+    } else if (imc <= 39.9){
         window.alert (`Obesidade grau II`);
-    }
-
-else (imc >= 40); {
+    } else {
     window.alert (`Obesidade grau III`);
 }
