c58fba0d14fadb0b0f99431c7e31395e566be542
Guilherme Alves <contatoguilhermealv@gmail.com>
2024-03-18 14:45:16 +0000

    Fix: arrumando os Desafios 1, 2 e 3

---
+++ desafio 3/desafio3.js
@@ -0,0 +1,22 @@
+/* Crie um conversor de temperatura entre as escalas Celsius, Fahrenheit e Kelvin. O usuário deve digitar a temperatura a ser convertida, a escala da temperatura atual e a escala para o qual o valor deve ser convertido.
+
+Entradas (3): valor atual da temperatura, escala atual da temperatura, escala para exibição (conversão).
+*/
+
+const
+valorTemperatura = window.prompt(`Valor da temperatura`),
+
+escalaTemperatura = window.prompt(`Informe qual escala de temperatura você você deseja converter, "Celsius, Fahrenheit ou Kelvin"`),
+
+conversao = window.prompt(`Informe para qual escala você deseja converter "Celsius, Fahrenheit ou Kelvin"`);
+
+const
+Fahrenheit = Fahrenheit;
+
+
+if (Fahrenheit = (valorTemperatura * 9/5) + 32){
+    window.alert(`${Fahrenheit}`)
+}
+
+
+
--- desafio1.js
+++ desafio1.js
@@ -1,17 +1,12 @@
-// Escreva um código em JavaScript que resolva o problema descrito no livro Lógica de Programação I, p. , em que temos que obter as 3 notas de um aluno, calcular a média e exibir se o aluno foi aprovado, reprovado ou se ficou para exame. Nãe é necessário ler as notas, você pode defini-las com variáveis.
+/* Escreva um código em JavaScript que resolva o problema descrito no livro Lógica de Programação I, p. , em que temos que obter as 3 notas de um aluno, calcular a média e exibir se o aluno foi aprovado, reprovado ou se ficou para exame. Nãe é necessário ler as notas, você pode defini-las com variáveis. */
 
-const nota1 = 5, nota2 = 5, nota3 = 5;
-const media = (nota1 + nota2 + nota3) / 3;
-
+const n1 = 8, n2 = 10, n3 = 5;
+const media = (n1 + n2 + n3) / 3;
 
 if (media >= 7) {
-    console.log(`aprovado`)
-}
-
-else if (media >= 5){
-    console.log(`exame`)
-}
-
-else {
-    console.log(`reprovado`)
+    console.log(`Aprovado com nota ${media.toFixed(1)}`);
+} else if (media >= 5) {
+    console.log(`Exame com nota ${media.toFixed(1)}`);
+} else {
+    console.log(`Reprovado ${media.toFixed(1)}`);
 }
