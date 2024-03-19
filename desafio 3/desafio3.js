eddb4a6699c46f5192d97148e8a72d03afa35be8
Guilherme Alves <contatoguilhermealv@gmail.com>
2024-03-19 14:27:24 +0000

    finalizando desafio 3

--- desafio 3/desafio3.js
+++ desafio 3/desafio3.js
@@ -4,19 +4,37 @@ Entradas (3): valor atual da temperatura, escala atual da temperatura, escala pa
 */
 
 const
-valorTemperatura = window.prompt(`Valor da temperatura`),
+valorTemperatura = parseFloat(window.prompt(`Valor da temperatura`)),
+atual = window.prompt(`Digite a temperatura atual "C, F ou K"`).toLowerCase(),
+conversao = window.prompt(`Informe para qual escala você deseja converter "C, F ou K"`).toLowerCase(),
 
-escalaTemperatura = window.prompt(`Informe qual escala de temperatura você você deseja converter, "Celsius, Fahrenheit ou Kelvin"`),
+CF = (valorTemperatura * 9/5) + 32,
+CK = valorTemperatura + 273.15,
+FC = (valorTemperatura - 32) * 5/9,
+FK = (valorTemperatura + 459.67) * 5/9,
+KC = (valorTemperatura - 273.15),
+KF = (valorTemperatura * 9/5) - 459.67;
 
-conversao = window.prompt(`Informe para qual escala você deseja converter "Celsius, Fahrenheit ou Kelvin"`);
-
-const
-Fahrenheit = Fahrenheit;
-
-
-if (Fahrenheit = (valorTemperatura * 9/5) + 32){
-    window.alert(`${Fahrenheit}`)
+if (atual == 'c' && conversao == 'f'){
+    window.alert(`Temperatura: ${CF}°F`)
 }
-
-
+else if (atual == 'c' && conversao == 'k'){
+    window.alert(`Temperatura: ${CK}`)
+}
+else if (atual == 'f' && conversao == 'c'){
+    window.alert(`Temperatura: ${FC}°`)
+}
+else if (atual == 'f' && conversao == 'k')
+{
+    window.alert(`Temperatura: ${FK}`)
+}
+else if (atual == 'k' && conversao == 'c'){
+    window.alert(`Temperatura: ${KC}°`)
+}
+else if (atual == 'k' && conversao == 'f'){
+    window.alert(`Temperatura:${KF}°F`)
+}
+else{
+    window.alert(`Temperatura não reconhecida`)
+}
 
