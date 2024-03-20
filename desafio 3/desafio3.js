b9eb2f44829c1fdab800421d9de610c33f298bb3
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-03-20 11:43:54 -0300

    revisão dos códigos

--- desafio 3/desafio3.js
+++ desafio 3/desafio3.js
@@ -5,9 +5,6 @@ Entradas (3): valor atual da temperatura, escala atual da temperatura, escala pa
 
 const
 valorTemperatura = parseFloat(window.prompt(`Valor da temperatura`)),
-atual = window.prompt(`Digite a temperatura atual "C, F ou K"`).toLowerCase(),
-conversao = window.prompt(`Informe para qual escala você deseja converter "C, F ou K"`).toLowerCase(),
-
 CF = (valorTemperatura * 9/5) + 32,
 CK = valorTemperatura + 273.15,
 FC = (valorTemperatura - 32) * 5/9,
@@ -15,26 +12,32 @@ FK = (valorTemperatura + 459.67) * 5/9,
 KC = (valorTemperatura - 273.15),
 KF = (valorTemperatura * 9/5) - 459.67;
 
-if (atual == 'c' && conversao == 'f'){
-    window.alert(`Temperatura: ${CF}°F`)
-}
-else if (atual == 'c' && conversao == 'k'){
-    window.alert(`Temperatura: ${CK}`)
-}
-else if (atual == 'f' && conversao == 'c'){
-    window.alert(`Temperatura: ${FC}°`)
-}
-else if (atual == 'f' && conversao == 'k')
-{
-    window.alert(`Temperatura: ${FK}`)
-}
-else if (atual == 'k' && conversao == 'c'){
-    window.alert(`Temperatura: ${KC}°`)
-}
-else if (atual == 'k' && conversao == 'f'){
-    window.alert(`Temperatura:${KF}°F`)
-}
-else{
-    window.alert(`Temperatura não reconhecida`)
-}
-
+if (!isNaN (valorTemperatura)){
+    
+    const
+    atual = window.prompt(`Digite a temperatura atual "C, F ou K"`).toLowerCase(),
+    conversao = window.prompt(`Informe para qual escala você deseja converter "C, F ou K"`).toLowerCase();
+     
+    if (atual == 'c' && conversao == 'f'){
+        window.alert(`Temperatura: ${CF.toFixed(2)}°F`)
+    }
+    else if (atual == 'c' && conversao == 'k'){
+        window.alert(`Temperatura: ${CK.toFixed(2)}`)
+    }
+    else if (atual == 'f' && conversao == 'c'){
+        window.alert(`Temperatura: ${FC.toFixed(2)}°`)
+    }
+    else if (atual == 'f' && conversao == 'k')
+    {
+        window.alert(`Temperatura: ${FK.toFixed(2)}`)
+    }
+    else if (atual == 'k' && conversao == 'c'){
+        window.alert(`Temperatura: ${KC.toFixed(2)}°`)
+    }
+    else if (atual == 'k' && conversao == 'f'){
+        window.alert(`Temperatura: ${KF.toFixed(2)}°F`)
+    }
+    
+}else{
+        window.alert(`Temperatura digitada não é um número válido ou você é um . Atualize a página!`)
+    }
