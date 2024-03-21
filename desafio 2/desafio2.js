72011b014fea7c06e34609b138f4eadb4ed1630a
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-03-21 11:53:12 -0300

    adaptando a calculadora de imc

--- desafio 2/desafio2.js
+++ desafio 2/desafio2.js
@@ -1,23 +1,22 @@
 function calculaIMC() {
     
     const
-        inf = window.alert(`Lembrando, que o cáculo de imc, não leva em consideração diversos fatores, como gênero, biotipo, saúde, se você pratica ou não esportes, por via das dúvidas consulte um médico.`),
-        peso = window.prompt(`Informe seu peso`),
-        altura = window.prompt(`Informe sua altura`),
-        imc= (peso / altura **2);
-    
-    if (imc <= 18.5){
-        window.alert(`IMC: ${imc.toFixed(1)} --> Abaixo do peso `);
-        } else if(imc <= 24.9){
-            window.alert(`IMC: ${imc.toFixed(1)} --> Peso normal`);
-        } else if(imc <= 29.9){
-            window.alert(`IMC: ${imc.toFixed(1)} --> Sobrepeso`);
-        } else if(imc <= 34.9){
-            window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau I`);
-        } else if (imc <= 39.9){
-            window.alert (`IMC: ${imc.toFixed(1)} --> Obesidade grau II`);
+    calculaIMC= (peso / altura **2);
+    let mensagem;
+
+    if (calculaIMC <= 18.5){
+        mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Abaixo do peso `;
+        } else if(calculaIMC <= 24.9){
+            mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Peso normal`;
+        } else if(calculaIMC <= 29.9){
+            mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Sobrepeso`;
+        } else if(calculaIMC <= 34.9){
+            mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Obesidade grau I`;
+        } else if (calculaIMC <= 39.9){
+            mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Obesidade grau II`;
         } else {
-            window.alert (`IMC: ${imc.toFixed(1)} --> Obesidade grau III`);
+            mensagem = `IMC: ${calculaIMC.toFixed(1)} --> Obesidade grau III`;
     }
     
+    return mensagem;
 }
