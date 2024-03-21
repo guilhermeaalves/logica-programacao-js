538f67e1ec05cb8705f618e029ccbd7bd2f56acb
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-03-21 20:34:44 -0300

    correção de erros do imc, e atualização do desafio 3

--- desafio 3/desafio3.js
+++ desafio 3/desafio3.js
@@ -6,13 +6,7 @@ Entradas (3): valor atual da temperatura, escala atual da temperatura, escala pa
 function converteTemperatura(){
 
     const
-    valorTemperatura = parseFloat(window.prompt(`Valor da temperatura`)),
-    CF = (valorTemperatura * 9/5) + 32,
-    CK = valorTemperatura + 273.15,
-    FC = (valorTemperatura - 32) * 5/9,
-    FK = (valorTemperatura + 459.67) * 5/9,
-    KC = (valorTemperatura - 273.15),
-    KF = (valorTemperatura * 9/5) - 459.67;
+    valorTemperatura = parseFloat (window.prompt(`Valor da temperatura`));
     
     if (!isNaN (valorTemperatura)){
         
@@ -20,25 +14,50 @@ function converteTemperatura(){
         atual = window.prompt(`Digite a temperatura atual "C, F ou K"`).toLowerCase(),
         conversao = window.prompt(`Informe para qual escala você deseja converter "C, F ou K"`).toLowerCase();
          
-        if (atual == 'c' && conversao == 'f'){
-            window.alert(`Temperatura: ${CF.toFixed(2)}°F`)
-        }
-        else if (atual == 'c' && conversao == 'k'){
-            window.alert(`Temperatura: ${CK.toFixed(2)}`)
-        }
-        else if (atual == 'f' && conversao == 'c'){
-            window.alert(`Temperatura: ${FC.toFixed(2)}°`)
-        }
-        else if (atual == 'f' && conversao == 'k')
-        {
-            window.alert(`Temperatura: ${FK.toFixed(2)}`)
-        }
-        else if (atual == 'k' && conversao == 'c'){
-            window.alert(`Temperatura: ${KC.toFixed(2)}°`)
-        }
-        else if (atual == 'k' && conversao == 'f'){
-            window.alert(`Temperatura: ${KF.toFixed(2)}°F`)
-        }
+        if (atual === 'c'){
+
+            if (conversao === 'k'){
+                window.alert(`${valorTemperatura} em ${atual} -> ${(valorTemperatura + 273.15) .toFixed(2)}`);
+            }
+
+            else if (conversao === 'f'){
+                window.alert(`${valorTemperatura} em ${atual} -> ${(valorTemperatura * 9/5 + 32)  .toFixed(2)}°F`);
+            }
+
+            else if (conversao === 'c'){
+                window.alert(`Escala selecionada: ${conversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`)
+            }
+        } else if (atual === 'f'){
+            
+            if (conversao === 'k'){
+                window.alert(`${valorTemperatura} em ${atual} -> ${(valorTemperatura + 459.67) * 5/9 .toFixed(2)}`)
+            }
+
+            else if (conversao === 'c'){
+                window.alert(`${valorTemperatura} em ${atual} -> ${((valorTemperatura - 32) * 5/9 .toFixed(2))}`);
+            }
+
+            else if (conversao === 'f'){
+                window.alert(`Escala selecionada: ${conversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`)
+            }    
+        }      
+        else if (atual === 'k'){
+
+                if (conversao === 'f'){
+                    window.alert(`${valorTemperatura} em ${atual} -> ${(valorTemperatura * 9/5 - 459.67 .toFixed(2))}`);
+                }
+
+                else if (conversao === 'c'){
+                    window.alert(`${valorTemperatura} em ${atual} -> ${(valorTemperatura - 273.15 .toFixed(2))}`);
+                }
+
+                else if (conversao === 'f'){
+                    window.alert(`Escala selecionada: ${conversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`)
+                }
+            }
+        
+        
+
         
     }else{
             window.alert(`Temperatura digitada não é um número válido ou você é um . Atualize a página!`)
