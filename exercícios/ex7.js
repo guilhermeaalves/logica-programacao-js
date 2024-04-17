e4d3c59b09ad33a19865f10c0f11d825245e10a1
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-04-17 11:42:37 -0300

    Feat: realização do exercício 7, uma calculadora de Taxa de Natalidade e Mortalidade.

---
+++ exercícios/ex7.js
@@ -0,0 +1,27 @@
+document.querySelector('#btClass').addEventListener('click', ()=>{
+
+    taxa = document.querySelector('#taxa').value.toLowerCase();
+    nascimentos = parseFloat(document.querySelector('#nascimentos').value);
+    habitantes = parseFloat(document.querySelector('#habitantes').value);
+    const resultado = taxaMouN(nascimentos, taxa, habitantes);
+
+    document.querySelector('#resultado').innerHTML = resultado;
+
+    function taxaMouN(nascimentos, taxa, habitantes){
+
+        if(taxa === 'n'){
+            return nascimentos * 1000 / habitantes;
+        }
+        else if(taxa === 'm'){
+            return nascimentos * 1000 / habitantes;
+        }
+        else{
+            return 'Comando digitado não existe, digite M ou N para escolher entre taxa de Mortalidade e taxa de Natalidade.'
+        }
+
+    }
+});
+
+document.querySelector('#btlimpa').addEventListener('click', function () {
+    document.querySelector('#resultado').innerHTML = "";
+});
